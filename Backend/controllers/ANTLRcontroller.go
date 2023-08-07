3a330f94b5022faa10b3e7d425cb1a8b1ea17b7f
Dgo45921 <3023135080101@ingenieria.usac.edu.gt>
2023-08-07 17:53:09 -0600

    fixing .gitignore

--- Backend/controllers/ANTLRcontroller.go
+++ Backend/controllers/ANTLRcontroller.go
@@ -15,19 +15,21 @@ func IndexRoute(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func Parse(w http.ResponseWriter, r *http.Request) {
-
+	// newCode is responsible to save the given input
 	var newCode models.SourceCode
+	// consoleResponse is responsible of returning all of the console logs
 	var consoleResponse models.ConsoleResponse
-
+	// getting the body from the request
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Fprintf(w, "ERROR")
 	}
+
+	// parsing the json
 	json.Unmarshal(reqBody, &newCode)
-
+	// printing the input
 	fmt.Println(newCode.Code)
-
+	// TODO just setting a response
 	consoleResponse.Console = "respuesta"
-
 	json.NewEncoder(w).Encode(consoleResponse)
 }
